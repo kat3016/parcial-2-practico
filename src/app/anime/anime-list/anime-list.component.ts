268f40b0f3f3c9ae4fbac2a2fa3f547df8b25a26
Nikol Rodriguez <nk.rodriguez@uniandes.edu.co>
2024-11-22 11:42:34 -0500

    Add total episodes and average rating display to anime list

--- src/app/anime/anime-list/anime-list.component.ts
+++ src/app/anime/anime-list/anime-list.component.ts
@@ -12,6 +12,10 @@ export class AnimeListComponent implements OnInit {
   selectedBAnime!: Anime;
   selected = false;
   animes: Array<Anime> = [];
+  num_episodes: number = 0;
+  totalRatings: number = 0;
+  
+
   constructor(private animeService: AnimeService) { }
 
   getAnimes(): void {
@@ -27,6 +31,19 @@ export class AnimeListComponent implements OnInit {
 
   ngOnInit() {
     this.getAnimes();
+    this.getTotalEpisodios();
+    this.getRatingPromedio();
   }
 
+  getTotalEpisodios(): void {
+    this.num_episodes = this.animes.reduce((total, anime) => total + anime.episode, 0);
+    
+  }
+
+ 
+  getRatingPromedio():void {
+    const num: number = this.animes.reduce((total, anime) => total + anime.Rating, 0);
+    this.totalRatings = num/ this.animes.length;
+    
+  }
 }
