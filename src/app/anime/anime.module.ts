b4170546cd31b3a32dd0c235496dbe6e7803af4f
Nikol Rodriguez <nk.rodriguez@uniandes.edu.co>
2024-11-22 12:13:49 -0500

    .

--- src/app/anime/anime.module.ts
+++ src/app/anime/anime.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AnimeListComponent } from './anime-list/anime-list.component';
 import { AnimeDetailComponent } from './anime-detail/anime-detail.component';
+import { AnimeRoutingModule } from './anime.routing';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule, AnimeRoutingModule, RouterModule
   ],
   exports: [AnimeListComponent],
   declarations: [AnimeListComponent, AnimeDetailComponent]
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { AnimeListComponent } from './anime/anime-list/anime-list.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component:  AnimeListComponent },
+  { path: 'books', loadChildren: () => import('./anime/anime.module').then(m => m.AnimeModule) },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
